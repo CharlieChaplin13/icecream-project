ef9da3227134c130f4d0a0c401a71141570b4364
CharlieChaplin13 <BlanshDyubua13@gmail.com>
2022-09-19 03:20:03 +0300

    update slick.js

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,7 +1,9 @@
 $('.one-time').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-  });
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  arrows: false,
+});
