120d142d3882094c2ad9ba12c38052eee23f4a0c
vad-mar <marunich.vad@gmail.com>
2022-09-21 21:00:24 +0300

    добавил стили для отображения моб+таб версий секции галлерея

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,9 +1,29 @@
-$('.gallery__list > .gallery__item:gt(0)').hide();
+$('.gallery__list--desk > .gallery__item--desk:gt(0)').hide();
 setInterval(function () {
-  $('.gallery__list > .gallery__item:first')
+  $('.gallery__list--desk > .gallery__item--desk:first')
     .fadeOut(1900) // скорость исчезания блока
     .next()
     .fadeIn(1200) // скорость появления блока
     .end()
-    .appendTo('.gallery__list');
-}, 4000); // скорость смены блоков
+    .appendTo('.gallery__list--desk');
+}, 3000); // скорость смены блоков
+
+$('.gallery__list--tab > .gallery__item--tab:gt(0)').hide();
+setInterval(function () {
+  $('.gallery__list--tab > .gallery__item--tab:first')
+    .fadeOut(1900) // скорость исчезания блока
+    .next()
+    .fadeIn(1200) // скорость появления блока
+    .end()
+    .appendTo('.gallery__list--tab');
+}, 3000); // скорость смены блоков
+
+$('.gallery__list--mob > .gallery__item--mob:gt(0)').hide();
+setInterval(function () {
+  $('.gallery__list--mob > .gallery__item--mob:first')
+    .fadeOut(1900) // скорость исчезания блока
+    .next()
+    .fadeIn(1200) // скорость появления блока
+    .end()
+    .appendTo('.gallery__list--mob');
+}, 3000); // скорость смены блоков
