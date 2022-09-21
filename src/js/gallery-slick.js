8891fde00c4386f637e098e0e04a79cd8fd0d533
vad-mar <marunich.vad@gmail.com>
2022-09-21 21:13:35 +0300

    Update gallery-slick.js

--- src/js/gallery-slick.js
+++ src/js/gallery-slick.js
@@ -1,9 +1,29 @@
-$("#slideshow > div:gt(0)").hide();
-        setInterval(function() {
-            $('#slideshow > div:first')
-                .fadeOut(500) // скорость исчезания блока
-                .next()
-                .fadeIn(1000) // скорость появления блока
-                .end()
-                .appendTo('#slideshow');
-        }, 3000); // скорость смены блоков
+$('.gallery__list--desk > .gallery__item--desk:gt(0)').hide();
+setInterval(function () {
+  $('.gallery__list--desk > .gallery__item--desk:first')
+    .fadeOut(1900) // скорость исчезания блока
+    .next()
+    .fadeIn(1200) // скорость появления блока
+    .end()
+    .appendTo('.gallery__list--desk');
+}, 3000); // скорость смены блоков
+
+$('.gallery__list--tab > .gallery__item--tab:gt(0)').hide();
+setInterval(function () {
+  $('.gallery__list--tab > .gallery__item--tab:first')
+    .fadeOut(1900) // скорость исчезания блока
+    .next()
+    .fadeIn(1200) // скорость появления блока
+    .end()
+    .appendTo('.gallery__list--tab');
+}, 3000); // скорость смены блоков
+
+$('.gallery__list--mob > .gallery__item--mob:gt(0)').hide();
+setInterval(function () {
+  $('.gallery__list--mob > .gallery__item--mob:first')
+    .fadeOut(1900) // скорость исчезания блока
+    .next()
+    .fadeIn(1200) // скорость появления блока
+    .end()
+    .appendTo('.gallery__list--mob');
+}, 3000); // скорость смены блоков
