459b69511b4a34ebefd194375471909f7a17b0ab
CharlieChaplin13 <101952904+CharlieChaplin13@users.noreply.github.com>
2022-09-21 18:43:32 +0000

    Deploying to gh-pages from @ CharlieChaplin13/icecream-project@d870df62ab455f02cdf321111d55561d36e29e4b 🚀

--- index.94377d18.js
+++ index.94377d18.js
@@ -1,2 +1,2 @@
 $(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,autoplay:!0,autoplaySpeed:4e3,arrows:!1});
-//# sourceMappingURL=index.486044c0.js.map
+//# sourceMappingURL=index.94377d18.js.map
--- index.cbff63d5.js
+++
@@ -1,2 +0,0 @@
-$("#slideshow > div:gt(0)").hide(),setInterval((function(){$("#slideshow > div:first").fadeOut(500).next().fadeIn(1e3).end().appendTo("#slideshow")}),3e3);
-//# sourceMappingURL=index.cbff63d5.js.map
