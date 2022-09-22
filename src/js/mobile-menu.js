e4c716b1081fb74433adfe1e9bb7019ef9e84b59
vad-mar <marunich.vad@gmail.com>
2022-09-22 15:23:20 +0300

    меню закрывается при переходе по вкладке

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -4,13 +4,17 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
+    menuList: document.querySelector('.mob-navigation__list'),
   };
-
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
+  refs.menuList.addEventListener('click', removeMenu);
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
+  }
+  function removeMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.remove('no-scroll');
   }
 })();
